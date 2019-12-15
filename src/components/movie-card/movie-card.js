0245f838e7b5eb50280c6d09467d19e234042512
Henrique Garretto <henrique.s.garretto@accenture.com>
2019-12-15 19:17:33 -0300

    Ajuste css listagem.

--- src/components/movie-card/movie-card.js
+++ src/components/movie-card/movie-card.js
@@ -6,7 +6,7 @@ import './movie-card.css';
 
 function MovieCard(props) {
   return (
-    <div className="MovieCard col-12 col-md-4 mb-5">
+    <div className="MovieCard col-12 col-md-4">
       {/* <NavLink to={props.detailUrl} >
         <img className="mw-100 mb-3"
           src={props.img}
@@ -14,7 +14,7 @@ function MovieCard(props) {
         />
       </NavLink> */}
       
-      <img className="mw-100 mb-3" src={'http://image.tmdb.org/t/p/w780' + props.imgUrl}/>
+      <img className="list-image mw-100 mb-3" src={'http://image.tmdb.org/t/p/w780' + props.imgUrl}/>
       <h3>{props.title}</h3>
       <span>{props.overview}</span>
     </div>
--- src/routes/list/List.js
+++ src/routes/list/List.js
@@ -9,14 +9,8 @@ function List() {
         <div className="container-fluid">
             <SearchBar />
             <MovieList/>
-            {/* <MovieCard name="teste" detailUrl="/detail" description="Testestestesteste" img="https://s2.glbimg.com/kuRU5wNsNZhTsYAYwdsGwt96Q9o=/0x0:976x549/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2019/Z/k/V0YjN0QDCq4BQiLqSj1w/bbcvialactea.jpg"/>
-            <MovieCard name="teste" detailUrl="/detail" description="Testestestesteste" img="https://s2.glbimg.com/kuRU5wNsNZhTsYAYwdsGwt96Q9o=/0x0:976x549/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2019/Z/k/V0YjN0QDCq4BQiLqSj1w/bbcvialactea.jpg"/>
-            <MovieCard name="teste" detailUrl="/detail" description="Testestestesteste" img="https://s2.glbimg.com/kuRU5wNsNZhTsYAYwdsGwt96Q9o=/0x0:976x549/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2019/Z/k/V0YjN0QDCq4BQiLqSj1w/bbcvialactea.jpg"/>
-            <MovieCard name="teste" detailUrl="/detail" description="Testestestesteste" img="https://s2.glbimg.com/kuRU5wNsNZhTsYAYwdsGwt96Q9o=/0x0:976x549/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2019/Z/k/V0YjN0QDCq4BQiLqSj1w/bbcvialactea.jpg"/>
-            <MovieCard name="teste" detailUrl="/detail" description="Testestestesteste" img="https://s2.glbimg.com/kuRU5wNsNZhTsYAYwdsGwt96Q9o=/0x0:976x549/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2019/Z/k/V0YjN0QDCq4BQiLqSj1w/bbcvialactea.jpg"/>
-            <MovieCard name="teste" detailUrl="/detail" description="Testestestesteste" img="https://s2.glbimg.com/kuRU5wNsNZhTsYAYwdsGwt96Q9o=/0x0:976x549/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2019/Z/k/V0YjN0QDCq4BQiLqSj1w/bbcvialactea.jpg"/> */}
-          </div>
         </div>
+    </div>
   );
 }
 
