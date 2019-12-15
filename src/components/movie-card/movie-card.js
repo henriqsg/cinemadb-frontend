870be23bb25aa3f7a1db52e325191f605d533826
adrianopb <adrianopbraga1@gmail.com>
2019-12-15 19:36:35 -0300

    details routing

--- src/components/movie-card/movie-card.js
+++ src/components/movie-card/movie-card.js
@@ -7,18 +7,13 @@ import './movie-card.css';
 function MovieCard(props) {
   return (
     <div className="MovieCard col-12 col-md-4">
-      {/* <NavLink to={props.detailUrl} >
-        <img className="mw-100 mb-3"
-          src={props.img}
-          alt={props.title}
-        />
-      </NavLink> */}
-      
-      <img className="list-image mw-100 mb-3" src={'http://image.tmdb.org/t/p/w780' + props.imgUrl}/>
-      <h3>{props.title}</h3>
-      <div className="overview">
-        <span>{props.overview}</span>
-      </div>
+      <NavLink to="/detail" >
+        <img className="list-image mw-100 mb-3" src={'http://image.tmdb.org/t/p/w780' + props.imgUrl}/>
+        <h3>{props.title}</h3>
+        <div className="overview">
+          <span>{props.overview}</span>
+        </div>
+      </NavLink>
     </div>
   );
 }
