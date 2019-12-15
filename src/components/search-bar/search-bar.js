85eadf67c5d05ab093dae902cd9ebeb7dc4b4b5c
Henrique Garretto <henrique.s.garretto@accenture.com>
2019-12-15 18:59:22 -0300

    Ajuste search bar.

--- src/components/search-bar/search-bar.js
+++ src/components/search-bar/search-bar.js
@@ -4,8 +4,10 @@ import './search-bar.css';
 export default class SearchBar extends React.Component {
     render () {
         return(
-        <div className="searchBar row mb-5 col-12">
-            <input type="text" placeholder="Search..." />
+        <div className="searchBar row">
+            <div className="mb-5 col-12">
+                <input type="text" placeholder="Search..." />
+            </div>
         </div>)
     }
 }
